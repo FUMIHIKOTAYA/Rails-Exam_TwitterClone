e77a9a5c224a0f54a017c7453bd15e36d29d15d1
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-07-24 16:15:27 +0900

    投稿確認画面の表示を設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,6 +34,10 @@ class PostsController < ApplicationController
     redirect_to posts_path, notice:'投稿を削除しました！'
   end
 
+  def confirm
+    @post = Post.new(post_params)
+  end
+
   private
 
   def post_params
