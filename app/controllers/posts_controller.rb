bc2be3341b794eeb69544e4e28934b1fe0a61be9
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2020-07-24 16:35:02 +0900

    投稿確認画面をnewアクションの時にだけ出す実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,10 +12,14 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    if @post.save
-      redirect_to posts_path, notice: "新たに投稿しました！"
-    else
+    if params[:back]
       render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "新たに投稿しました！"
+      else
+        render :new
+      end
     end
   end
 
